11f48def6591d948fed55ab9d24a74d10c4eed8c
nicoboy11 <even.sosa@gmail.com>
2017-07-12 18:01:28 +0100

    thumbs

--- Code/esnapi/functions/dataMgr.js
+++ Code/esnapi/functions/dataMgr.js
@@ -44,91 +44,155 @@ var self = module.exports = {
 
             return true;
         },
-    
-    responseMsg: function(mensaje){
-        return JSON.stringify({"message":mensaje});
-    },
+    /** responseMsg -
+     * 
+     */
+        responseMsg: function(mensaje){
+            return JSON.stringify({"message":mensaje});
+        },
 
-    handleResponse: function(result,res,errorMessage){
+    /** handleResponse -
+     * 
+     */
+        handleResponse: function(result,res,errorMessage){
 
-        if(errorMessage == undefined) errorMessage = "";
+            if(errorMessage == undefined) errorMessage = "";
 
-        if(result != undefined && result[0] != undefined ){
-            if(result[0].length == 0){
-                if(errorMessage == ""){
-                    res.status(200).end( "[]" );
+            if(result != undefined && result[0] != undefined ){
+                if(result[0].length == 0){
+                    if(errorMessage == ""){
+                        res.status(200).end( "[]" );
+                    }
+                    else{
+                        res.status(401).end( self.responseMsg(errorMessage) );
+                    }
                 }
                 else{
-                    res.status(401).end( self.responseMsg(errorMessage) );
+                    res.status(200).end( JSON.stringify(result[0]) );
                 }
             }
             else{
-                res.status(200).end( JSON.stringify(result[0]) );
+                res.status(200).end( self.responseMsg("Ok") );
             }
-        }
-        else{
-            res.status(200).end( self.responseMsg("Ok") );
-        }
-    },
+        },
 
-    /**
+    /** reqUpload - Upload files
      * 
      * @param {The http request} req 
      * @param {What is the file for? (avatar, post, post message, group, Task, Task Message, Chat)} type 
      * @param {What field needs to be read to name the uploaded file} field 
      * @param {Callback function} callback
      */
-    reqUpload: function(req, type, field, callback){
-        /**
+        reqUpload: function(req, type, field, callback){
+            /**
 
-        file structure
+            file structure
 
-        uploads/
+            uploads/
 
-            avatars: avt_[personId]
-            
-            photo in post: ph_pst_[postId]
-            file in post:  fl_pst_[postId]
+                avatars: avt_[personId]
+                
+                photo in post: ph_pst_[postId]
+                file in post:  fl_pst_[postId]
 
-            photo in post message: ph_pstm_[postMessageId]
-            file in post message:  fl_pstm_[postMessageId]
+                photo in post message: ph_pstm_[postMessageId]
+                file in post message:  fl_pstm_[postMessageId]
 
-            photo in group: ph_grp_[groupId]
-            file in group: fl_grp_[groupId]
-            photo in task: ph_tk_[taskMessageId]
-            file in task: fl_tk_[taskMessageId]
-            photo in chat: ph_ch_[postId]
-            file in task: fl_ch_[postId]
-        */
+                photo in group: ph_grp_[groupId]
+                file in group: fl_grp_[groupId]
+                photo in task: ph_tk_[taskMessageId]
+                file in task: fl_tk_[taskMessageId]
+                photo in chat: ph_ch_[postId]
+                file in task: fl_ch_[postId]
+            */
 
-        var form = new formidable.IncomingForm();
-        var fileName = '';
-        var uniq = new Date().getTime() / 1000;
-        var params = {};
+            var form = new formidable.IncomingForm();
+            var fileName = '';
+            var uniq = new Date().getTime() / 1000;
+            var params = {};
 
-        form.parse(req);
-        form.on('field', function(name, value){
-            params[name] = value;
-        }) 
-        .on('fileBegin',function(name, file){
-            fileName = config.prefixes[type]
-            fileName = fileName + (isNaN(field)?params[field]:field) + '_' + uniq + '.' + file.type.split('/')[1];
-            file.path = './uploads/' + fileName;
-            params['fileName'] = fileName;
-            console.log('file created!');
-        })
-        .on('progress',function(bytesReceived, bytesExpected){
-            console.log('progress: ' + bytesReceived + ' : ' + bytesExpected);
-        })        
-        .on('end',function(){
-            if(fileName === ''){
-                fileName = undefined;
-            }
-            callback(fileName, params);
-        })
-        .on('error', function(err){
-            console.log('upload Error:' + err.message);
-            callback();
-        })
-    }    
+            form.parse(req);
+            form.on('field', function(name, value){
+                params[name] = value;
+            }) 
+            .on('fileBegin',function(name, file){
+                fileName = config.prefixes[type]
+                fileName = fileName + (isNaN(field)?params[field]:field) + '_' + uniq + '.' + file.type.split('/')[1];
+                file.path = './uploads/' + fileName;
+                params['fileName'] = fileName;
+                console.log('file created!');
+            })
+            .on('progress',function(bytesReceived, bytesExpected){
+                console.log('progress: ' + bytesReceived + ' : ' + bytesExpected);
+            })        
+            .on('end',function(){
+                if(fileName === ''){
+                    fileName = undefined;
+                } else {//If file exists - create a thumb copy
+                    if(type=='avatar') {
+                        self.resize(fileName, 'small');
+                        self.resize(fileName, 'big');
+                    } else {
+                        self.dequalify(fileName);
+                    }
+                }
+
+                callback(fileName, params);
+            })
+            .on('error', function(err){
+                console.log('upload Error:' + err.message);
+                callback();
+            })
+        },
+
+        resize: function (fileName, size) {
+            require('lwip').open('./uploads/' + fileName, function(err, image) {
+                if(err) {
+                    console.log('Could not generate thumb');
+                } else {
+                    var maxWidth;
+                    
+                    switch(size) {
+                        case 'small':
+                            maxWidth = 96
+                            break;
+                        case 'big':
+                            maxWidth = 349
+                            break;
+                        default:
+                            maxWidth = 44
+                            break;
+                    }
+
+                    image.batch()
+                        .cover(maxWidth,maxWidth)
+                        .writeFile('./uploads/thumbs/' + size + '/' + fileName, function(err) {
+                            if(err) {
+                                console.log('Could not save thumb');
+                            } else {
+                                console.log('Thumb saved');
+                            }
+                        });                             
+                }
+            });           
+        },
+
+       dequalify: function (fileName) {
+            require('lwip').open('./uploads/' + fileName, function(err, image) {
+                if(err) {
+                    console.log('Could not generate thumb');
+                } else {
+                    image.batch()
+                        .cover(200,200)
+                        .blur(5)
+                        .writeFile('./uploads/thumbs/blured/' + fileName, function(err) {
+                            if(err) {
+                                console.log('Could not save blur');
+                            } else {
+                                console.log('Blur saved');
+                            }
+                        });                             
+                }
+            });  
+       }       
 }
